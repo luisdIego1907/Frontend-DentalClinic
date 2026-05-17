18029c4d4480d9499fe3a370c0501ce7a854dd6c
luisdIego1907 <luisdiegoquesada197@gmail.com>
2026-05-16 23:59:14 -0600

    cambio en home y se creo la interfaz patient

---
+++ Dental_Clinic/src/data/patient.ts
@@ -0,0 +1,11 @@
+export interface PatientData {
+  identification: string;
+  first_name: string;
+  last_name: string;
+  birth_date: string;
+  phone: string;
+  email: string;
+  address: string;
+  gender: string;
+  status: string;
+}
--- Dental_Clinic/src/pages/patients/RegisterPatient.jsx
+++ Dental_Clinic/src/pages/patients/RegisterPatient.jsx
@@ -1,20 +1,16 @@
 import { useState } from "react";
 import PatientForm from "../../components/patients/PatientForm";
+import type { PatientData } from "../../data/patient";
 
 export default function RegisterPatient() {
-
   const [successMessage, setSuccessMessage] = useState("");
 
-  const handleSavePatient = async (patientData) => {
-
+  const handleSavePatient = async (patientData: PatientData) => {
     try {
-
-      await registerPatient(patientData);
+      console.log("Paciente a registrar:", patientData);
 
       setSuccessMessage("Paciente registrado correctamente.");
-
     } catch (error) {
-
       console.error("Error al registrar paciente:", error);
     }
   };
@@ -23,9 +19,7 @@ export default function RegisterPatient() {
     <main>
       <h1>Registrar Paciente</h1>
 
-      {successMessage && (
-        <p className="success-message">{successMessage}</p>
-      )}
+      {successMessage && <p className="success-message">{successMessage}</p>}
 
       <PatientForm onSubmit={handleSavePatient} />
     </main>
