68b9956756f91cd6314a591e0dae0b2676daf0ea
AndreaPao16 <paolagr561@gmail.com>
2026-05-20 19:55:44 -0600

    Footer visible con enlace a redes sociales y texto institucional

--- Dental_Clinic/src/App.tsx
+++ Dental_Clinic/src/App.tsx
@@ -1,15 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Footer } from "./shared/Footer";
+import './App.css';
 
 function App() {
   return (
-    <div className="">
+    <div className="min-h-screen flex flex-col bg-slate-50">
       <BrowserRouter>
-        <Header />
 
-        <main className="flex-1 flex flex-col">
+        <main className="flex-1 flex flex-col items-center justify-center">
           <Routes>
-   
+            <Route
+              path="/"
+              element={
+                <h1 className="text-4xl font-bold text-sky-500">
+                  Clínica Dental
+                </h1>
+              }
+            />
           </Routes>
         </main>
 
