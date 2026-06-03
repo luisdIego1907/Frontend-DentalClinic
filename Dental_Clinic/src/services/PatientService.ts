52a6286ded1c047524dc38cb1721e3c6e160afde
luisdIego1907 <luisdiegoquesada197@gmail.com>
2026-06-03 15:31:07 -0600

    En PatientService se agrego para hacer POST a la backend. Se agrego dicha funcinalidad a RegisterPatient

--- Dental_Clinic/src/services/PatientService.ts
+++ Dental_Clinic/src/services/PatientService.ts
@@ -1,23 +1,21 @@
-import type { PatientDetails } from "../data/patient";
-import {config} from "../config"
+import type { PatientDetails, PatientData } from "../data/patient";
+import { config } from "../config";
 
 const API_URL = `${config.api.url}/api/patients`;
 
-export async function getPatients() : Promise<PatientDetails[]>{
+export async function getPatients(): Promise<PatientDetails[]> {
+  try {
+    const response = await fetch(API_URL);
 
-    try{
-
-        const response = await fetch(API_URL);
-
-        if(!response.ok){
-            throw new Error("Error al obtener los productos");
-        }
-
-        return await response.json();
-    } catch(error){
-        console.error("Error en patientservice: " , error);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Error al obtener los productos");
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error en patientservice: ", error);
+    throw error;
+  }
 }
 
 export async function getPatientById(id: number): Promise<PatientDetails> {
@@ -31,6 +29,31 @@ export async function getPatientById(id: number): Promise<PatientDetails> {
     return await response.json();
   } catch (error) {
     console.error("Error en getPatientById:", error);
+    throw error;
+  }
+}
+
+export async function createPatient(patientData: PatientData): Promise<PatientDetails> {
+  
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(patientData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al crear el paciente");
+    }
+
+    return await response.json();
+
+  } catch (error) {
+    console.error("Error en createPatient", error);
     throw error;
   }
 }
